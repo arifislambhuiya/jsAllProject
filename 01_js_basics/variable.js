268fee1492e5fac7d9bb15a240d27466c75c7ed4
Ariful Islam <48514575+arifislambhuiya@users.noreply.github.com>
2024-11-16 15:47:34 +0000

    add dev container

---
+++ 01_js_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 94844
+let  accountEmail = "arifulislam@gmail.com"
+var accountParrword = 'Pass1234'
+accountCity = 'Narsingdi'
+
+// accountId = 10
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountParrword,accountCity])
